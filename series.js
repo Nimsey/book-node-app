621d168ae1f83f069b86901ebe12be1cb06a6d18
susan stephens <crazeygrafix@gmail.com>
2023-10-17 16:25:50 -0600

    chore: in better understanding the loops i write it out in normal for loop

--- series.js
+++ series.js
@@ -70,6 +70,43 @@ function findAuthorsByLetter(letter) {
     return matchingAuthors;
 }
 
+// function findAuthorsByLetter(letter) {
+//     // Combine both book series into an array
+//     const allBooks = [...Object.values(bookSeries_setOne), ...Object.values(bookSeries_setTwo)];
+//     const matchingAuthors = [];
+
+//     for (let i = 0; i < allBooks.length; i++) {
+//         const authorName = allBooks[i].author;
+
+//         if (authorName.charAt(0).toLowerCase() === letter.toLowerCase()) {
+//             matchingAuthors.push(authorName);
+//         }
+//     }
+
+//     return matchingAuthors;
+// }
+
+// Example usage:
+const letter = "s"; // Replace with the desired letter
+const authors = findAuthorsByLetter(letter);
+
+if (authors.length > 0) {
+    console.log(`Authors whose names start with the letter "${letter}":`);
+    console.log(authors);
+} else {
+    console.log(`No authors found whose names start with the letter "${letter}".`);
+}
+
+
+function findAuthorsByLetter2(letter) {
+    const bookSeriesArray = [bookSeries_setOne, bookSeries_setTwo];
+    const matchingAuthors = [];
+
+    for (const series of bookSeriesArray) {
+        const authorName = series[bookKey].author;
+    }
+}
+
 module.exports = {
     bookSeries_setOne,
     bookSeries_setTwo,
