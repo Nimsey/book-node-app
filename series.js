7ebbe7fabe9f9b65e3c40e3f11a0b12b4084eb3d
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-10-17 20:17:41 -0700

    feat: add 3 new paths (genre/fiction, genre/nonfiction) and ISBN lookup

--- series.js
+++ series.js
@@ -4,18 +4,21 @@ const bookSeries_setOne = {
         author: "a author",
         coverImage: "url of image",
         pageCount: 321,
+        genre: "fiction"
     },
     book2: {
         name: "title two",
         author: "b author",
         coverImage: "url of image",
         pageCount: 411,
+        genre: "fiction"
     },
     book3: {
         name: "title three",
         author: "c author",
         coverImage: "url of image",
         pageCount: 621,
+        genre: "fiction"
     },
 };
 
@@ -25,33 +28,42 @@ const bookSeries_setTwo = {
         author: "a author",
         coverImage: "url of image",
         pageCount: 321,
+        genre: "fiction"
     },
     book2: {
         name: "title two",
         author: "b author",
         coverImage: "url of image",
         pageCount: 411,
+        genre: "fiction"
     },
     book3: {
         name: "title three",
         author: "c author",
         coverImage: "url of image",
         pageCount: 621,
+        genre: "fiction"
     },
     book4: {
         name: "title four",
         author: "d author",
         coverImage: "url of image",
         pageCount: 502,
+        genre: "fiction"
     },
     book5: {
         name: "title five",
         author: "e author",
         coverImage: "url of image",
         pageCount: 555,
+        genre: "fiction"
     },
 };
 
+const books = [
+    ...Object.values(bookSeries_setOne),
+    ...Object.values(bookSeries_setTwo)
+];
 
 function findAuthorsByLetter(letter) {
     const bookSeriesArray = [bookSeries_setOne, bookSeries_setTwo];
@@ -97,6 +109,20 @@ if (authors.length > 0) {
     console.log(`No authors found whose names start with the letter "${letter}".`);
 }
 
+function findBookByISBN(isbn) {
+    return ('test ISBN lookup.  Submitted number:', isbn)
+}
+
+function listBooksByGenre(genre) {
+    const booksInGenre = [];
+
+for (const book of books) {
+    if (book.genre === genre) {
+        booksInGenre.push(book);
+    }
+}
+return booksInGenre;
+}
 
 function findAuthorsByLetter2(letter) {
     const bookSeriesArray = [bookSeries_setOne, bookSeries_setTwo];
@@ -110,5 +136,7 @@ function findAuthorsByLetter2(letter) {
 module.exports = {
     bookSeries_setOne,
     bookSeries_setTwo,
-    findAuthorsByLetter
+    findAuthorsByLetter,
+    findBookByISBN,
+    listBooksByGenre
 }
